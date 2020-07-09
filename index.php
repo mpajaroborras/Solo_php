db0f29868c9a16f1bc60dd54b0bd1aa3e7a77158
roluguz <67868633+roluguz@users.noreply.github.com>
2020-07-09 11:14:27 -0500

    Add files via upload
    lo subio rodolfo

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>pag-Educativa</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Educacion virtual">
+	  <meta name="keywords" content="Educacion, cursos, notas">
+    <meta name="author" content="">
+    <link rel="stylesheet" href="component/css/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="component/css/fonts.css">
+    <link rel="stylesheet" href="component/css/estilos.css">
+    <link rel="stylesheet" href="component/css/style.css"> 
+  </head>
+  <body>
+    <header>
+      <div class="contenedor">
+        <div class="row">
+          <div class="col-md-8 col-sm-8 col-xs-8">
+            <div id="marca">
+              <h1><span class="resaltado">Educacion</span> virtual</h1>          
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-4 col-xs-4">
+            <a href ='moodle38/index.php'id="start"  class="btn btn-xs btn-success">Ingresar</a>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div id="particles-js"></div>
+    <section id="boletin">
+      <div class="contenedor">
+        <div class="row">
+          <div class="col-md-8 col-sm-8 col-xs-8">        
+            <h1>Agiganta tu conocimiento</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="cajas">
+      <div class="contenedor">
+        <div class="caja">
+          <img src="component/img/responsive.png">
+          <h3>Desarrollo  Web</h3>
+          <p>Aprende el Diseño Responsive adaptable al tamaño de los diferentes dispositivos Pcs, Tablets, Celulares.</p>
+        </div>
+        <div class="caja">
+          <img src="component/img/aplicaciones.png">
+          <h3>Desarrollo de Apps Android</h3>
+          <p>Desarrollo y Diseño de Aplicaciones Moviles Android-Ios</p>
+        </div>
+        <div class="caja">
+          <img src="component/img/diseno.png">
+          <h3>Diseño Gráfico</h3>
+          <p>Aprende a Personalizar  Marcas, con Logo, Slogan y Marketing Digital. Publicidad,Pendones,Avisos,tarjetas de Presentación y Redes sociales.</p>
+        </div>
+      </div>
+    </section>
+    <footer>
+      <p>EDUCACION VIRTUAL, Copyright &copy; 2020</p>
+    </footer>
+    <script src="component/css/jQuery/jquery.min.js"></script>    
+    <script src="component/css/jQuery/jquery.slim.min.js"></script>    
+    <script src="component/css/bootstrap/js/bootstrap.min.js"></script>   
+    <script type="text/javascript" src="component/js/particles.js"></script>
+    <script type="text/javascript" src="component/js/app.js"></script>
+  </body>
+</html>
